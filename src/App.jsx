94bf847006438b87b2951f9da1fa9f1367d03c4d
salman-itShaala <salman.shaikh@itshaala.com>
2024-12-18 12:37:28 +0530

    added delete and mark as done features

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { addTodo } from "./slices/todoSlice";
+import { addTodo, deleteTodo, markAsDone } from "./slices/todoSlice";
 import { useState } from "react";
 import { nanoid } from "@reduxjs/toolkit";
 
@@ -19,6 +19,7 @@ function App() {
     };
 
     dispatch(addTodo(todoObj));
+    setTask("");
   }
 
   return (
@@ -32,6 +33,7 @@ function App() {
           placeholder="Enter Todo"
           className="px-8 py-4 rounded-md"
           onChange={(e) => setTask(e.target.value)}
+          value={task}
         />
         <button
           className="border border-white px-8 py-4 rounded-md font-bold text-slate-200
@@ -44,33 +46,32 @@ function App() {
       <div className="flex justify-between w-full px-32 min-h-[70vh]">
         <div className="flex flex-col gap-4 w-1/2">
           {todos.map((todo) => {
-            return (
-              <TodoComponent
-                key={todo.id}
-                title={todo.todo}
-                id={todo.id}
-                done={todo.done}
-              />
-            );
+            if (!todo.done) {
+              return (
+                <TodoComponent
+                  key={todo.id}
+                  title={todo.todo}
+                  id={todo.id}
+                  done={todo.done}
+                />
+              );
+            }
           })}
         </div>
-        <div className="w-[1px] bg-slate-400"></div>
+        <div className="w-[1px] bg-slate-400 mx-4"></div>
         <div className="flex flex-col gap-4 w-1/2">
-          <div className="flex gap-8 text-slate-50 items-center">
-            <p>Learn HTML, CSS and Js</p>
-            <button
-              className="border border-green-400 px-4 py-2 rounded-md font-bold text-green-400
-        hover:bg-slate-500"
-            >
-              Incomplete
-            </button>
-            <button
-              className="border border-red-400 px-4 py-2 rounded-md font-bold text-red-400
-        hover:bg-slate-500"
-            >
-              Delete
-            </button>
-          </div>
+          {todos.map((todo) => {
+            if (todo.done) {
+              return (
+                <TodoComponent
+                  key={todo.id}
+                  title={todo.todo}
+                  id={todo.id}
+                  done={todo.done}
+                />
+              );
+            }
+          })}
         </div>
       </div>
     </div>
@@ -78,18 +79,30 @@ function App() {
 }
 
 function TodoComponent({ title, id, done }) {
+  const dispatch = useDispatch();
+
+  function handleDeleteTodo(id) {
+    dispatch(deleteTodo(id));
+  }
+
+  function handleDone(id) {
+    dispatch(markAsDone(id));
+  }
+
   return (
     <div className="flex gap-8 text-slate-50 items-center">
       <p className="w-1/2">{title} </p>
       <button
         className="border border-green-400 px-4 py-2 rounded-md font-bold text-green-400
         hover:bg-slate-500"
+        onClick={() => handleDone(id)}
       >
         Done
       </button>
       <button
         className="border border-red-400 px-4 py-2 rounded-md font-bold text-red-400
         hover:bg-slate-500"
+        onClick={() => handleDeleteTodo(id)}
       >
         Delete
       </button>
--- src/slices/todoSlice.js
+++ src/slices/todoSlice.js
@@ -9,8 +9,37 @@ const todoSlice = createSlice({
     addTodo: (state, action) => {
       state.tasks.push(action.payload); // {id: nanoid() , todo: "Learn something new today", done: false}
     },
+    deleteTodo: (state, action) => {
+      const id = action.payload;
+
+      state.tasks = state.tasks.filter((task) => task.id !== id);
+
+      /**
+       * 
+      const tasksArray = state.tasks;
+
+      const filterdArray = [];
+      for (let i = 0; i < tasksArray.length; i++) {
+        if (id !== tasksArray[i].id) {
+          filterdArray.push(tasksArray[i]);
+        }
+      }
+
+      state.tasks = filterdArray;
+       */
+    },
+    markAsDone: (state, action) => {
+      const id = action.payload;
+
+      state.tasks = state.tasks.map((task) => {
+        if (task.id === id) {
+          task.done = !task.done;
+        }
+        return task;
+      });
+    },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo, markAsDone } = todoSlice.actions;
 export const todoReducer = todoSlice.reducer;
